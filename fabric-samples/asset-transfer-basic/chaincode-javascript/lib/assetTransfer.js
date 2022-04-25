70d1217191b146edbb6a736736db6103f40a7fcc
Roman Bitarovský <56035158+Roman-24@users.noreply.github.com>
2022-04-25 21:42:15 +0200

    other basic func done

--- fabric-samples/asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js
+++ fabric-samples/asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js
@@ -63,18 +63,21 @@ class AssetTransfer extends Contract {
         const mspid = ctx.clientIdentity.getMSPID();
         const orgName = mspid.split('MSP')[0];
 
-        // generate random int form 0 to 999
-        const randomInt = Math.floor(Math.random() * 1000);
-        // https://stackoverflow.com/questions/1127905/how-can-i-format-an-integer-to-a-specific-length-in-javascript
-        randomInt = randomInt.toString().padStart(3, '0');
+        do {
+            // generate random int form 0 to 999
+            const randomInt = Math.floor(Math.random() * 1000);
+            // https://stackoverflow.com/questions/1127905/how-can-i-format-an-integer-to-a-specific-length-in-javascript
+            randomInt = randomInt.toString().padStart(3, '0');
 
-        if (orgName == 'Org1') {
-            const flightName = 'EC';
-        } else if ( orgName == 'Org2' ) {
-            const flightName = 'BS';
-        }
+            if (orgName == 'Org1') {
+                const flightName = 'EC';
+            } else if ( orgName == 'Org2' ) {
+                const flightName = 'BS';
+            }
 
-        const flightNr = flightName + randomInt;
+            const flightNr = flightName + randomInt;
+        } while (await this.AssetExists(ctx, flightNr) == false);
+        
 
         const flight = {
             flightNr: flightNr,
@@ -84,63 +87,51 @@ class AssetTransfer extends Contract {
             availablePlaces: availablePlaces,
         };
 
-        //we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
+        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
         await ctx.stub.putState(flightNr, Buffer.from(stringify(sortKeysRecursive(flight))));
         return JSON.stringify(flight);
     }
 
     // ReadAsset returns the asset stored in the world state with given id.
-    async ReadAsset(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
+    async ReadAsset(ctx, flightNr) {
+
+        // get the asset from chaincode state
+        const assetJSON = await ctx.stub.getState(flightNr); 
         if (!assetJSON || assetJSON.length === 0) {
-            throw new Error(`The asset ${id} does not exist`);
+            throw new Error(`The asset ${flightNr} does not exist`);
         }
         return assetJSON.toString();
     }
 
     // UpdateAsset updates an existing asset in the world state with provided parameters.
-    async UpdateAsset(ctx, id, color, size, owner, appraisedValue) {
-        const exists = await this.AssetExists(ctx, id);
-        if (!exists) {
-            throw new Error(`The asset ${id} does not exist`);
+    async UpdateAsset(ctx, flightNr, flyFrom, flyTo, dateTimeDeparture, availablePlaces) {
+
+        // asset must exist for update
+        const exists = await this.AssetExists(ctx, flightNr);
+        if (exists == false) {
+            throw new Error(`The asset ${flightNr} does not exist`);
         }
 
         // overwriting original asset with new asset
-        const updatedAsset = {
-            ID: id,
-            Color: color,
-            Size: size,
-            Owner: owner,
-            AppraisedValue: appraisedValue,
+        const updatedFlight = {
+            flightNr: flightNr,
+            flyFrom: flyFrom,
+            flyTo: flyTo,
+            dateTimeDeparture: dateTimeDeparture,
+            availablePlaces: availablePlaces,
         };
         // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        return ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
+        return ctx.stub.putState(flightNr, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
     }
 
     // DeleteAsset deletes an given asset from the world state.
-    async DeleteAsset(ctx, id) {
-        const exists = await this.AssetExists(ctx, id);
-        if (!exists) {
-            throw new Error(`The asset ${id} does not exist`);
+    async DeleteAsset(ctx, flightNr) {
+
+        const exists = await this.AssetExists(ctx, flightNr);
+        if (exists == false) {
+            throw new Error(`The asset ${flightNr} does not exist`);
         }
-        return ctx.stub.deleteState(id);
-    }
-
-    // AssetExists returns true when asset with given ID exists in world state.
-    async AssetExists(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id);
-        return assetJSON && assetJSON.length > 0;
-    }
-
-    // TransferAsset updates the owner field of asset with given id in the world state.
-    async TransferAsset(ctx, id, newOwner) {
-        const assetString = await this.ReadAsset(ctx, id);
-        const asset = JSON.parse(assetString);
-        const oldOwner = asset.Owner;
-        asset.Owner = newOwner;
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
-        return oldOwner;
+        return ctx.stub.deleteState(flightNr);
     }
 
     // GetAllAssets returns all assets found in the world state.
@@ -164,6 +155,27 @@ class AssetTransfer extends Contract {
         return JSON.stringify(allResults);
     }
 
+    async reserveSeats(ctx, flightNr, number) {
+    
+    }
+
+    async bookSeats(ctx, reservationNr) {
+
+    }
+
+    async checkIn(ctx, reservationNr, passportIDs[]) {
+
+    } 
+
+    // ======================== HELPER FUNCTIONS ========================
+
+    // AssetExists returns true when asset with given ID exists in world state.
+    async AssetExists(ctx, flightNr) {
+
+        // get the asset from chaincode state
+        const assetJSON = await ctx.stub.getState(flightNr); 
+        return assetJSON && assetJSON.length > 0;
+    }
 
     // isOrganization returns true if the function caller is an organization.
     isOrganization(ctx) {
